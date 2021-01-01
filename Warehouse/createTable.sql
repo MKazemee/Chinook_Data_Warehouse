792e160685d4a385107ecde1e57e5465a3d4d735
MKazemee <48950543+MKazemee@users.noreply.github.com>
2021-01-01 20:20:56 +0330

    Update createTable.sql

--- Warehouse/createTable.sql
+++ Warehouse/createTable.sql
@@ -23,3 +23,36 @@ CREATE TABLE [dbo].[Dim_Location]
     [City] NVARCHAR(50),
     [Country] NVARCHAR(50),
 );
+
+
+CREATE TABLE [dbo].[Dim_Album]
+(
+    [AlbumId] INT PRIMARY KEY NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [ArtistId] INT NOT NULL,
+	[ArtistName] NVARCHAR(130)
+);
+
+CREATE TABLE DataWarehouse.[dbo].[Dim_Track]
+(
+	[Id] BIGINT PRIMARY KEY IDENTITY(1,1),  --Surrogate key for SCD2
+    [TrackId] INT NOT NULL,
+    [Name] NVARCHAR(210) NOT NULL,
+    [AlbumId] INT,
+	[AlbumTitle] NVARCHAR(160) NOT NULL,
+    [ArtistId] INT NOT NULL,
+	[ArtistName] NVARCHAR(130),
+    [MediaTypeId] INT NOT NULL,
+	[MediaTypeName] NVARCHAR(130),
+    [GenreId] INT,
+	[GenreName] NVARCHAR(130),
+    [Composer] NVARCHAR(220),
+    [Milliseconds] INT NOT NULL,
+    [Bytes] INT,
+    [UnitPrice] NUMERIC(20,2) NOT NULL,
+	[AddDate] DATE,
+	----SCD2
+	[Start_Date_UnitPrice] DATE,
+	[End_Date_UnitPrice] DATE,
+	[Current_Flag_UnitPrice] bit
+);
